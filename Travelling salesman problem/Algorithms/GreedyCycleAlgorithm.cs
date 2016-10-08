be9f9321ffc127492ffa15dad44f3c7c217f2143
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-10-08 18:14:31 +0200

    working greedy cycle algorithm

--- Travelling salesman problem/Algorithms/GreedyCycleAlgorithm.cs
+++ Travelling salesman problem/Algorithms/GreedyCycleAlgorithm.cs
@@ -6,15 +6,17 @@ namespace ConsoleApplication.Algorithms
 {
     public class GreedyCycleAlgorithm : IAlgorithm
     {
-        private int _steps {get;}
+        private int _steps { get; }
 
-        public GreedyCycleAlgorithm(int steps){
+        public GreedyCycleAlgorithm(int steps)
+        {
             _steps = steps;
         }
         public int Solve(int startNode, Graph graph, out IList<int> path)
         {
             path = new List<int>();
-            if(_steps<1){
+            if (_steps < 1)
+            {
                 return 0;
             }
             graph.CurrentNode = startNode;
@@ -38,38 +40,56 @@ namespace ConsoleApplication.Algorithms
 
             while (--steps > 0)
             {
-                int bestConnection1 = -1;
-                int bestBefore = -1;
-                int newCost1 = -1;
-                for(int node = 0 ; node < (path.Count-2) ; node++)
+                Result bestResult = new Result();
+                for (int node = 0; node < (path.Count - 2); node++)
                 {
-                    int firstNode = path[node];
-                    int secondNode = path[node+1];
-                    int newCost = -1;
-                    int bestConnection = -1;
-                    foreach(int unvisitedNode in unvisitedNodes){
-                        int cost = graph.Cost(firstNode, unvisitedNode) + graph.Cost(unvisitedNode, secondNode);
-                        if(newCost < 0 || cost < newCost){
-                            newCost = cost;
-                            bestConnection = unvisitedNode;
-                        }
-                    }
-                    if(newCost1 < 0 || newCost < newCost1){
-                        bestConnection1 = bestConnection;
-                        bestBefore = node+1;
-                        newCost1 = newCost;
+                    var localBestResult = getBestResult(path[node], path[node + 1], unvisitedNodes, graph);
+                    if (bestResult.cost < 0 || localBestResult.cost < bestResult.cost)
+                    {
+                        bestResult = localBestResult;
+                        bestResult.followingNode = node + 1;
                     }
                 }
-                int before = path[bestBefore-1];
-                int after = path[bestBefore];
-                path.Insert(bestBefore, bestConnection1);
-                int oldCost = graph.Cost(before, after);
-                int newestCost = graph.Cost(before, bestConnection1) + graph.Cost(bestConnection1, after);
-                pathCost -= oldCost;
-                pathCost += newestCost;
-                unvisitedNodes.Remove(bestConnection1);
+                int previousNode = path[bestResult.followingNode - 1];
+                int followingNode = path[bestResult.followingNode];
+                path.Insert(bestResult.followingNode, bestResult.bestNode);
+                pathCost -= graph.Cost(previousNode, followingNode);
+                pathCost += graph.Cost(previousNode, bestResult.bestNode) + graph.Cost(bestResult.bestNode, followingNode);
+                unvisitedNodes.Remove(bestResult.bestNode);
             }
             return pathCost;
         }
+
+        private Result getBestResult(int firstNode, int secondNode, List<int> unvisitedNodes, Graph graph)
+        {
+            Result result = new Result();
+
+            foreach (int unvisitedNode in unvisitedNodes)
+            {
+                int previousCost = graph.Cost(firstNode, secondNode);
+                int cost = graph.Cost(firstNode, unvisitedNode) + graph.Cost(unvisitedNode, secondNode);
+                int costDifference = cost - previousCost;
+                if (result.cost < 0 || costDifference < result.cost)
+                {
+                    result.cost = costDifference;
+                    result.bestNode = unvisitedNode;
+                }
+            }
+            return result;
+        }
+    }
+
+    class Result
+    {
+
+        public Result()
+        {
+            bestNode = -1;
+            cost = -1;
+            followingNode = -1;
+        }
+        public int bestNode { get; set; }
+        public int cost { get; set; }
+        public int followingNode { get; set; }
     }
 }
