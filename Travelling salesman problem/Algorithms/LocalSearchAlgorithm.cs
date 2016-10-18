9981e1604b19d88729a13a2eeadc889ec85a186d
Rafał Araszkiewicz <r.araszkiewicz@ktr.local>
2016-10-18 15:57:16 +0200

    Fix some syntax flaws

--- Travelling salesman problem/Algorithms/LocalSearchAlgorithm.cs
+++ Travelling salesman problem/Algorithms/LocalSearchAlgorithm.cs
@@ -1,10 +1,23 @@
 ﻿using ConsoleApplication.Graphs;
 using System.Collections.Generic;
 using System.Linq;
-using System;
 
 namespace ConsoleApplication.Algorithms
 {
+	/// <summary>
+	/// Reprezentuje ruch wykonany w algorytmie 
+	/// </summary>
+	internal struct LocalSearchMove
+	{
+		public int CostDifference { get; set; }
+
+		public int FirstPointIndex { get; set; }
+
+		public int SecondPointIndex { get; set; }
+
+		public LocalSearchStrategy Strategy { get; set; }
+	}
+
 	public class LocalSearchAlgorithm : AlgorithmBase
 	{
 		public LocalSearchAlgorithm(int steps, IEdgeFinder edgeFinder) : base(steps, edgeFinder)
@@ -19,20 +32,22 @@ namespace ConsoleApplication.Algorithms
 			while (currentCostIncrese < 0)
 			{
 				var bestMove = FindBestMove(path, completeGraph);
-				if(bestMove.Strategy == LocalSearchStrategy.VERTICES){
+				if (bestMove.Strategy == LocalSearchStrategy.Vertices)
+				{
 					//flip VERTICES
 					// FIRST - node you want to exclude from path
 					// SECOND - node you want to add to path
-					int nodetoExcludeIndex = bestMove.FirstPointIndex;
+					var nodetoExcludeIndex = bestMove.FirstPointIndex;
 					path[nodetoExcludeIndex] = bestMove.SecondPointIndex;
-				} else
+				}
+				else
 				{
 					//flip EDGES
-					List<int> output = new List<int>(path);
-					int length = bestMove.SecondPointIndex - bestMove.FirstPointIndex;
-					List<int> path1 = output.GetRange(0, bestMove.FirstPointIndex);
-					List<int> path2 = output.GetRange(bestMove.FirstPointIndex+1, length);
-					List<int> path3 = output.GetRange(bestMove.SecondPointIndex+1, output.Count - length);
+					var output = new List<int>(path);
+					var length = bestMove.SecondPointIndex - bestMove.FirstPointIndex;
+					var path1 = output.GetRange(0, bestMove.FirstPointIndex);
+					var path2 = output.GetRange(bestMove.FirstPointIndex + 1, length);
+					var path3 = output.GetRange(bestMove.SecondPointIndex + 1, output.Count - length);
 					path2.Reverse();
 					path.Clear();
 					output.AddRange(path1);
@@ -45,74 +60,75 @@ namespace ConsoleApplication.Algorithms
 			return solution.Aggregate(0, (accu, ele) => accu += ele);
 		}
 
-		private Move FindBestMove(IList<int> path, IGraph completeGraph)
+		private LocalSearchMove FindBestEdgeFlip(IList<int> path, IGraph completeGraph)
 		{
-			Move bestVertice = FindBestVerticeFlip(path, completeGraph);
-			Move bestEdge = FindBestEdgeFlip(path, completeGraph);
+			LocalSearchMove bestLocalSearchMove = new LocalSearchMove();
+			bestLocalSearchMove.Strategy = LocalSearchStrategy.Edges;
 
-			if(bestEdge.CostDifference > 0 && bestVertice.CostDifference > 0)
+			for (int i = 0; i < path.Count - 1; i++)
+			{
+				for (int j = i; j < path.Count - 1; j++)
+				{
+					int currentCost = completeGraph.Weight(path[i], path[i + 1]) + completeGraph.Weight(path[j], path[j + 1]);
+					int newCost = completeGraph.Weight(path[i], path[j]) + completeGraph.Weight(path[i + 1], path[j + 1]);
+					int newCostDifference = newCost - currentCost;
+					if (newCost < currentCost && bestLocalSearchMove.CostDifference < newCostDifference)
+					{
+						bestLocalSearchMove.CostDifference = newCostDifference;
+						bestLocalSearchMove.FirstPointIndex = i;
+						bestLocalSearchMove.SecondPointIndex = j;
+					}
+				}
+			}
+
+			return bestLocalSearchMove;
+		}
+
+		private LocalSearchMove FindBestMove(IList<int> path, IGraph completeGraph)
+		{
+			LocalSearchMove bestVertice = FindBestVerticeFlip(path, completeGraph);
+			LocalSearchMove bestEdge = FindBestEdgeFlip(path, completeGraph);
+
+			if (bestEdge.CostDifference > 0 && bestVertice.CostDifference > 0)
 				return null;
 
-			if(bestEdge.CostDifference < bestVertice.CostDifference)
+			if (bestEdge.CostDifference < bestVertice.CostDifference)
 				return bestEdge;
 			return bestVertice;
 		}
 
-        private Move FindBestVerticeFlip(IList<int> path, IGraph completeGraph)
-        {
-			Move bestMove = new Move();
-			bestMove.Strategy = LocalSearchStrategy.VERTICES;
-			for (var pointIndex = 1; pointIndex < (path.Count - 1); pointIndex++){
-				int indexBefore = path[pointIndex-1];
+		private LocalSearchMove FindBestVerticeFlip(IList<int> path, IGraph completeGraph)
+		{
+			LocalSearchMove bestLocalSearchMove = new LocalSearchMove();
+			bestLocalSearchMove.Strategy = LocalSearchStrategy.Vertices;
+			for (var pointIndex = 1; pointIndex < (path.Count - 1); pointIndex++)
+			{
+				int indexBefore = path[pointIndex - 1];
 				int index = path[pointIndex];
-				int indexAfter = path[pointIndex+1];
+				int indexAfter = path[pointIndex + 1];
 				int currentCost = completeGraph.Weight(indexBefore, index) + completeGraph.Weight(index, indexAfter);
 				var unvisitedNodes = completeGraph.Nodes.Where(node => !path.Contains(node)).ToList();
-				for(int i = 0; i < unvisitedNodes.Count; i++){
+				for (int i = 0; i < unvisitedNodes.Count; i++)
+				{
 					int newNode = unvisitedNodes[i];
 					int newCost = completeGraph.Weight(indexBefore, newNode) + completeGraph.Weight(newNode, indexAfter);
 					int newCostDifference = newCost - currentCost;
-					if(newCost < currentCost && bestMove.CostDifference < newCostDifference){
-						bestMove.CostDifference = newCostDifference;
-						bestMove.FirstPointIndex = pointIndex; // node you want to exclude from path
-						bestMove.SecondPointIndex = unvisitedNodes[i]; // node you want to add to path
+					if (newCost < currentCost && bestLocalSearchMove.CostDifference < newCostDifference)
+					{
+						bestLocalSearchMove.CostDifference = newCostDifference;
+						bestLocalSearchMove.FirstPointIndex = pointIndex; // node you want to exclude from path
+						bestLocalSearchMove.SecondPointIndex = unvisitedNodes[i]; // node you want to add to path
 					}
 				}
 			}
 			//TODO - include path indexees groups: last, fist, second and one before last, last, first
-			return bestMove;
-        }
-
-        private Move FindBestEdgeFlip(IList<int> path, IGraph completeGraph)
-        {
-			Move bestMove = new Move();
-			bestMove.Strategy = LocalSearchStrategy.EDGES;
-			
-			for(int i=0; i<path.Count - 1; i++){
-				for(int j = i; j<path.Count - 1; j++){
-					int currentCost = completeGraph.Weight(path[i], path[i+1]) + completeGraph.Weight(path[j], path[j+1]);
-					int newCost = completeGraph.Weight(path[i], path[j]) + completeGraph.Weight(path[i+1], path[j+1]);
-					int newCostDifference = newCost - currentCost;
-					if(newCost < currentCost && bestMove.CostDifference < newCostDifference){
-						bestMove.CostDifference = newCostDifference;
-						bestMove.FirstPointIndex = i; 
-						bestMove.SecondPointIndex = j;
-					}
-				}
-			}
-
-			return bestMove;
-        }
-    }
-
-	internal class Move
-	{
-		public int FirstPointIndex {get; set;}
-        public int SecondPointIndex{get; set;}
-        public int CostDifference{get; set;}
-
-        public LocalSearchStrategy Strategy{get; set;}
+			return bestLocalSearchMove;
+		}
 	}
 
-	internal enum LocalSearchStrategy  { VERTICES, EDGES};
+	internal enum LocalSearchStrategy
+	{
+		Vertices,
+		Edges
+	};
 }
--- Travelling salesman problem/Program.cs
+++ Travelling salesman problem/Program.cs
@@ -10,44 +10,47 @@ using System.IO;
 
 namespace ConsoleApplication
 {
-    public class Program
-    {
-        private const int Steps = 50;
+	public class Program
+	{
+		private const int Steps = 50;
 
-        public static void Main(string[] args)
-        {
-            var buildConfig = BuildConfiguration();
-            var dataPath = buildConfig[$"{nameof(AppConfiguration)}:{nameof(AppConfiguration.GraphDataPath)}"];
-            var coordinatesPath = buildConfig[$"{nameof(AppConfiguration)}:{nameof(AppConfiguration.GraphCoordinatesPath)}"];
-            var dataLoader = new GraphLoader(dataPath, 100);
-            var graph = dataLoader.Load();
+		public static void Main(string[] args)
+		{
+			var buildConfig = BuildConfiguration();
+			var dataPath = buildConfig[$"{nameof(AppConfiguration)}:{nameof(AppConfiguration.GraphDataPath)}"];
+			var coordinatesPath = buildConfig[$"{nameof(AppConfiguration)}:{nameof(AppConfiguration.GraphCoordinatesPath)}"];
+			var dataLoader = new GraphLoader(dataPath, 100);
+			var graph = dataLoader.Load();
 
-            var solver = new TspSolver(graph);
+			var solver = new TspSolver(graph);
 
-            var resultPrinter = new ResultPrinter()
-                .AddPrinter(new ConsolePrinter(), new ConsoleContentBuilder(solver))
-                .AddPrinter(new FilePrinter($"{DateTime.Now.Date.ToFileTime()}_results.txt"), new FileContentBuilder(solver, coordinatesPath));
+			var resultPrinter = new ResultPrinter()
+				.AddPrinter(new ConsolePrinter(), new ConsoleContentBuilder(solver))
+				.AddPrinter(new FilePrinter($"{DateTime.Now.Date.ToFileTime()}_results.txt"), new FileContentBuilder(solver, coordinatesPath));
 
-            SolveAndPrint(solver, new NearestNeighborAlgorithm(Steps, new EdgeFinder()), "NEAREST NEIGHBOR", resultPrinter);
+			SolveAndPrint(solver, new NearestNeighborAlgorithm(Steps, new EdgeFinder()), "NEAREST NEIGHBOR", resultPrinter);
 
-            SolveAndPrint(solver, new GreedyCycleAlgorithm(Steps, new EdgeFinder()), "GREEDY CYCLE", resultPrinter);
+			SolveAndPrint(solver, new GreedyCycleAlgorithm(Steps, new EdgeFinder()), "GREEDY CYCLE", resultPrinter);
 
-            SolveAndPrint(solver, new NearestNeighborAlgorithm(Steps, new GraspEdgeFinder(3)), "NEAREST NEIGHBOR GRASP", resultPrinter);
+			SolveAndPrint(solver, new NearestNeighborAlgorithm(Steps, new GraspEdgeFinder(3)), "NEAREST NEIGHBOR GRASP", resultPrinter);
 
-            SolveAndPrint(solver, new GreedyCycleAlgorithm(Steps, new GraspEdgeFinder(3)), "GREEDY CYCLE GRASP", resultPrinter);
+			SolveAndPrint(solver, new GreedyCycleAlgorithm(Steps, new GraspEdgeFinder(3)), "GREEDY CYCLE GRASP", resultPrinter);
 
-            //Console.ReadKey();
-        }
+			SolveAndPrint(new TspLocalSearchSolver(graph, solver, new NearestNeighborAlgorithm(Steps, new EdgeFinder())), new LocalSearchAlgorithm(Steps, new EdgeFinder()),
+				"NN with local search opt", resultPrinter);
 
-        private static IConfigurationRoot BuildConfiguration() => new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("config.json")
-            .Build();
+			//Console.ReadKey();
+		}
 
-        private static void SolveAndPrint(ISolver solver, IAlgorithm algorithm, string title, IResultPrinter resultPrinter)
-        {
-            solver.Solve(algorithm);
-            resultPrinter.Print(title);
-        }
-    }
+		private static IConfigurationRoot BuildConfiguration() => new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("config.json")
+			.Build();
+
+		private static void SolveAndPrint(ISolver solver, IAlgorithm algorithm, string title, IResultPrinter resultPrinter)
+		{
+			solver.Solve(algorithm);
+			resultPrinter.Print(title);
+		}
+	}
 }
