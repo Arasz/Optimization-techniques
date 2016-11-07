f9d2b5f3507e004172f45d0b22fb268fe762e74e
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-11-07 22:19:44 +0100

    Add edge similarity implementation

--- Travelling salesman problem/Similarity/EdgeSimillarityStrategy.cs
+++ Travelling salesman problem/Similarity/EdgeSimillarityStrategy.cs
@@ -4,6 +4,21 @@ namespace ConsoleApplication.Similarity
 {
     public class EdgeSimillarityStrategy : ISimilarityCalculationStrategy
     {
-        public double CalculateSimilarity(Path first, Path second) => 0;
+        public double CalculateSimilarity(Path first, Path second)
+        {
+            int similarityCounter;
+            // we assum that paths have the same length
+            for (var n = 0; n < first.Count-1; n++)
+            {
+                if (first.Nodes[n] == second.Nodes[n] && first.Nodes[n + 1] == second.Nodes[n + 1])
+                    similarityCounter++;
+                else if (first.Nodes[n+1] == second.Nodes[n] && first.Nodes[n] == second.Nodes[n + 1])
+                    similarityCounter++;
+                else if (first.Nodes[n] == second.Nodes[n+1] && first.Nodes[n+1] == second.Nodes[n])
+                    similarityCounter++;
+            }
+            return similarityCounter;
+        }
+
     }
 }
